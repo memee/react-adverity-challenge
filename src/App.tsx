96de4c7dac80c73e93157ec5e023f811d53096f0
Maciej Maciaszek <maciej.maciaszek@gmail.com>
2020-08-24 08:36:45 +0200

    Add circular progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState, useMemo } from "react";
-import { Container, Grid, Paper, makeStyles } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 import _ from "lodash";
 
 import "./App.css";
@@ -31,12 +35,15 @@ export default function App() {
     dataSources: [],
     campaigns: [],
   } as Api.CompoundFilters);
+  const [isLoading, setIsLoading] = useState(false);
   const filteredData = useMemo(() => {
     return Api.getFilteredTimeSeries(data, filters);
   }, [filters, data]);
 
   useEffect(() => {
+    setIsLoading(true);
     Api.fetchData().then(({ data, dataSources }: Api.State) => {
+      setIsLoading(false);
       setData(data);
       setDataSources(dataSources);
     });
@@ -56,7 +63,13 @@ export default function App() {
         <Grid item xs={9}>
           <Paper className={classes.visualizer}>
             {_.isEmpty(data) ? (
-              <h2>No data!</h2>
+              isLoading ? (
+                <Grid container justify="center">
+                  <CircularProgress color="secondary" />
+                </Grid>
+              ) : (
+                <h2>No data!</h2>
+              )
             ) : (
               <DataVisualizer
                 data={filteredData}
